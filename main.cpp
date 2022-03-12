fa333b074f66147548a21f91ed8ead993120a22c
Sergei Johansen <sergeitheprogrammer@gmail.com>
2022-03-12 21:33:52 +0100

    Part 1 - done

--- main.cpp
+++ main.cpp
@@ -32,9 +32,16 @@ struct Acc2d
 // Random
 
 float random(float min, float max) {
-	 std::random_device rnddev;
-	 std::mt19937 gen(rnddev());
-	 std::uniform_real_distribution<float> dist(min, max);
+	static std::random_device rnddev;
+	static std::mt19937 gen(rnddev());
+	static std::uniform_real_distribution<float> dist(min, max);
+
+	return dist(gen);
+}
+float random2(float min, float max) {
+	static std::random_device rnddev;
+	static std::mt19937 gen(rnddev());
+	static std::uniform_real_distribution<float> dist(min, max);
 
 	return dist(gen);
 }
@@ -45,7 +52,7 @@ float random(float min, float max) {
 // Constants
 const float a = 10.0f;
 const float pi = 3.14f;
-const Acc2d acc = { 0.0f, 120.0f };
+const Acc2d acc = { 0.0f, 5.0f };
 
 class SpawningBall
 {
@@ -59,12 +66,14 @@ public:
 
 	float initSpeed;
 	Vel2d initVel;
+	float timeSinceSpawn;
 
 	void initVariables(float x, float y, float speed, float ang)
 	{
-		this->color = sf::Color::Magenta;
-		this->radius = 20.f;
+		this->color = sf::Color::Green;
+		this->radius = 10.f;
 		this->distFromCenter = 0;
+		this->timeSinceSpawn = 0;
 
 		// Spawn position
 		pos = { x, y };
@@ -123,39 +132,51 @@ int main()
 		// Time
 		float dt = clock.restart().asSeconds();
 		t += dt;
-		srand(t);
+		// srand(t);
 		// dt/60 is time spent on one frame
 
 		// Updating
 
 		// 1. Add 1 ball
-		balls.push_back(new SpawningBall(x_center, y_center, static_cast<float>(rand() % 5) + 20.0f, random(0.0f, 2.0f * pi)));
+		balls.push_back(new SpawningBall(x_center, y_center, random2(1.f, 10.f), -random(pi/2.5f, pi - pi/2.5f)));
+		balls.push_back(new SpawningBall(x_center, y_center, random2(1.f, 10.f), -random(pi / 2.5f, pi - pi / 2.5f)));
+		balls.push_back(new SpawningBall(x_center, y_center, random2(1.f, 10.f), -random(pi / 2.5f, pi - pi / 2.5f)));
 		
 		// 2. Update the balls' pos
 		for (int i = 0; i < balls.size(); i++)
 		{
-		
-			balls[i]->pos.x += balls[i]->initVel.x * dt;
-			balls[i]->pos.y += acc.y * dt * dt + balls[i]->initVel.y * dt;
-			
+			balls[i]->timeSinceSpawn += dt;
+			balls[i]->pos.x = balls[i]->pos.x + balls[i]->initVel.x * balls[i]->timeSinceSpawn;
+			balls[i]->pos.y = balls[i]->pos.y + acc.y/2 * balls[i]->timeSinceSpawn 
+				* balls[i]->timeSinceSpawn + balls[i]->initVel.y
+				* balls[i]->timeSinceSpawn;
 
-			// 3. Delete balls out of screen
+			balls[i]->ball.setPosition(balls[i]->pos.x, balls[i]->pos.y);
+
+			cout << balls[i]->initSpeed << endl;
+		}
+			
+		// 3. Delete balls out of screen
+		for (int i = 0; i < balls.size(); i++)
+		{
+
 			if ((balls[i]->pos.x + balls[i]->radius) < 0 ||
 				(balls[i]->pos.x - balls[i]->radius) > window.getSize().x ||
 				(balls[i]->pos.y + balls[i]->radius) < 0 ||
 				(balls[i]->pos.y - balls[i]->radius) > window.getSize().y)
 			{
+				delete balls[i];
 				balls.erase(balls.begin() + i);
 			}
-
 		}
 
+
+		// cout << balls.size() << endl;
 		window.clear();
 
 		// Drawing
 		for (int i = 0; i < balls.size(); i++)
 		{
-			balls[i]->ball.setPosition(balls[i]->pos.x, balls[i]->pos.y);
 			window.draw(balls[i]->ball);
 		}
 
